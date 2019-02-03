f0292452f3ab425a25c6d884da30b39e986f34c8
zhaoangus <zhao_angus@163.com>
2019-02-03 16:29:14 +0800

    form v1

---
+++ src/components/form/Form.vue
@@ -0,0 +1,71 @@
+<template>
+  <form>
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'iForm',
+  props: {
+    model: {
+      type: Object
+    },
+    rules: {
+      type: Object
+    }
+  },
+  provide () {
+    return {
+      form: this
+    }
+  },
+  data () {
+    return {
+      fields: []
+    }
+  },
+  methods: {
+    // 公开方法：全部重置数据
+    resetFields () {
+      this.fields.forEach(field => {
+        field.resetField()
+      })
+    },
+    // 公开方法：全部校验数据，支持Promise
+    validate (callback) {
+      return new Promise(resolve => {
+        let valid = true
+        let count = 0
+        this.fields.forEach(field => {
+          // 空字符串表示所有规则都检验
+          field.validate('', errors => {
+            if (errors) {
+              valid = false
+            }
+            if (++count === this.fields.length) {
+              // 全部完成
+              resolve(valid)
+              if (typeof callback === 'function') {
+                callback(valid)
+              }
+            }
+          })
+        })
+      })
+    }
+  },
+  created () {
+    this.$on('on-form-item-add', (field) => {
+      if (field) this.fields.push(field)
+    })
+    this.$on('on-form-item-remove', (field) => {
+      if (field) this.fields.splice(this.fields.indexOf(field), 1)
+    })
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
